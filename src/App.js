dbab4d006a4d13660b86a1563ebe745cea07cbe4
gyawali9 <roshangyawali9@gmail..com>
2022-03-05 00:18:59 +0545

    Nested components and code organized

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react'
-import ExpenseItem from './components/ExpenseItem'
+import Expenses from './components/Expenses/Expenses'
 
 const App = () => {
+
+  let expenses = [
+    {id: 'e1', title: 'School Fee', amount: 250,  date: new Date(2021, 5, 12)},
+    {id: 'e2', title: 'Books', amount: 150,  date: new Date(2021, 2, 14)},
+    {id: 'e3', title: 'House Rent', amount: 1500,  date: new Date(2022, 5, 2)},
+    {id: 'e4', title: 'Food', amount: 2050,  date: new Date(2022, 2, 2)},
+  ];
+
   return (
     <>
       <h1>Let's get started</h1>
-    <ExpenseItem/>
+      <Expenses item={expenses}/>
     </>
   )
 }
