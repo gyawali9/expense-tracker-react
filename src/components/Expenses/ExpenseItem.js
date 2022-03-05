e8b820288d6b5c43eb26d9cfbe26d27ac628927b
gyawali9 <roshangyawali9@gmail..com>
2022-03-05 13:09:48 +0545

    click and change event implemented

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,18 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ExpenseDate from './ExpenseDate'
 import Card from '../UI/Card'
 import './ExpenseItem.css'
 
 const ExpenseItem = (props) => {
+  const [newTitle, setNewTitle] = useState("");
+  const [title, setTitle] = useState(props.title);
+
+  const clickHandler = () =>{
+    setTitle(newTitle);
+  }
+
+  const changeHandler = (e) =>{
+    setNewTitle(e.target.value)
+  }
 
     
   return (
     <Card className='expense-item'>
         <ExpenseDate date={props.date} /> 
         <div className='expense-item__description'>
-            <h2>{props.title}</h2>
+            <h2>{title}</h2>
             <div className='expense-item__price'>${props.amount}</div>
         </div>
+        <input onChange={changeHandler} type="text" value={newTitle} />
+        <button onClick={clickHandler}>Change Title</button>
     </Card>
   )
 }
